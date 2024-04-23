7c2cc30fe53c52405becdd256a96378c6cdd84ef
Ilyas Khisyametdinov <hopi@mail.ru>
2024-04-23 13:56:52 +0400

    CARS-11 desktop 1024 done with slider

--- script.js
+++ script.js
@@ -21,3 +21,42 @@ for (let i = 0; i < links.length; i++) {
     document.body.classList.remove("lock");
   });
 }
+
+// слайдер
+
+let slides = document.querySelectorAll(".slide");
+let slideControlLeft = document.querySelector(".left-button");
+let slideControlRight = document.querySelector(".right-button");
+let dot = document.querySelectorAll(".dot");
+
+slides[0].style.opacity = "1";
+dot[0].style.backgroundColor = "#0EC261";
+let currentIndex = 0;
+
+let switchSlide = () => {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.opacity = "0";
+    dot[i].style.backgroundColor = "#fff";
+  }
+  slides[currentIndex].style.opacity = "1";
+  dot[currentIndex].style.backgroundColor = "#0EC261";
+};
+
+slideControlLeft.onclick = function () {
+  if (currentIndex === 0) {
+    currentIndex = slides.length - 1;
+    switchSlide();
+  } else {
+    currentIndex--;
+    switchSlide();
+  }
+};
+slideControlRight.onclick = function () {
+  if (currentIndex === slides.length - 1) {
+    currentIndex = 0;
+    switchSlide();
+  } else {
+    currentIndex++;
+    switchSlide();
+  }
+};
